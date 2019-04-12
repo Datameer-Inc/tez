3cc8c9e680a0f5b6dad3caf6901862eab50f3c73
Matthias Fuhrmeister <mfuhrmeister@datameer.com>
2019-04-12 15:27:47 +0200

    DAP-24985 Add support for Azure Data Lake Store as Datameer private folder storage

--- tez-api/src/main/java/org/apache/tez/client/TezClientUtils.java
+++ tez-api/src/main/java/org/apache/tez/client/TezClientUtils.java
@@ -337,7 +337,8 @@ public class TezClientUtils {
     if (fs.exists(stagingArea)) {
       FileStatus fsStatus = fs.getFileStatus(stagingArea);
       String owner = fsStatus.getOwner();
-      if (!(owner.equals(currentUser) || owner.equals(realUser))) {
+      boolean checkOwner = conf.getBoolean("datameer.tez.check.owner", true);
+      if (checkOwner && !(owner.equals(currentUser) || owner.equals(realUser))) {
         throw new IOException("The ownership on the staging directory "
             + stagingArea + " is not as expected. " + "It is owned by " + owner
             + ". The directory must " + "be owned by the submitter "
